45368cd915499553960510492cc59f4709c6e45e
Dan <dan@localhost.localdomain>
2019-01-10 02:08:28 -0800

    script added

---
+++ s3-setup.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+LX_BASE=""
+LX_VERSION=""
+
+if [ -r /etc/os-release ]; then
+    . /etc/os-release
+	if [ $ID = arch ]; then
+		LX_BASE=$ID
+    elif [ $ID = ubuntu ]; then
+		LX_BASE=$ID
+		LX_VERSION=$VERSION_ID
+	elif [ ! -z "$UBUNTU_CODENAME" ] ; then
+		LX_BASE="ubuntu"
+		LX_VERSION=$VERSION_ID
+    else
+		LX_BASE=$ID
+		LX_VERSION=$VERSION
+    fi
+else
+    echo "Could not identify your distro. Please open script and run commands manually."
+	exit
+fi
+
+SUR_MODEL="$(dmidecode | grep "Product Name" -m 1 | xargs | sed -e 's/Product Name: //g')"
+SUR_SKU="$(dmidecode | grep "SKU Number" -m 1 | xargs | sed -e 's/SKU Number: //g')"
+
+echo "\nRunning $LX_BASE version $LX_VERSION on a $SUR_MODEL.\n"
+
+read -rp "Press enter if this is correct, or CTRL-C to cancel." cont;echo
+
+echo "\nContinuing setup...\n"
+
+echo "Getting dump of your ACPI DSDT table...\n"
+cat /sys/firmware/acpi/tables/DSDT > dsdt.aml
+
+echo "Decompiling dump...\n"
+iasl -d dsdt.aml
+
+echo "Applying patch...\n"
+patch --verbose < x1_dsdt.patch
+
+echo "\nAll done! Please reboot."
