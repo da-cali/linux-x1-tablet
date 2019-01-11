0a9f14f89439708502f6ca2cd9c85914673432c3
Dan <dan@localhost.localdomain>
2019-01-10 19:42:39 -0800

    script messages updated

--- s3-setup.sh
+++ s3-setup.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 
-echo "Getting dump of your ACPI DSDT table...\n"
+echo "Getting dump of your ACPI DSDT table... \n"
 cat /sys/firmware/acpi/tables/DSDT > dsdt.aml
 
-echo "Decompiling dump...\n"
+echo "Decompiling dump... \n"
 iasl -d dsdt.aml
 
-echo "Applying patch...\n"
+echo "Applying patch... \n"
 patch --verbose < x1_dsdt.patch
 
-echo "Recompiling dsdt...\n"
+echo "Recompiling dsdt... \n"
 iasl -ve -tc dsdt.dsl
 
-echo "WARNING: Continue only if there were no errors in the previous steps.\n"
+echo "WARNING: Continue only if there were no errors in the previous steps! \n"
 read -rp "Do you want to continue? (type yes or no) " continueCopy;echo
 
 if [ "$continueCopy" = "yes" ]; then
-		echo "Copying dsdt to boot...\n"
+		echo "Copying dsdt to boot... \n"
 		cp dsdt.aml /boot
 
-		echo "Copying custom acpi loader to grub folder...\n"
+		echo "Copying custom acpi loader to grub folder... \n"
 		cp 01_acpi /etc/grub.d
 
-		echo "Making loader executable...\n"
+		echo "Making loader executable... \n"
 		chmod 0755 /etc/grub.d/01_acpi
 
-		echo "\nAll done. Please open /etc/default/grub and add 'mem_sleep_default=deep' 		to the GRUB_CMDLINE_LINUX. Then update your grub and reboot."	
+		echo "All done. Please open /etc/default/grub and add 'mem_sleep_default=deep' to the GRUB_CMDLINE_LINUX. Then update your grub and reboot."	
 else
-	echo "Aborting\n"
+	echo "Aborting \n"
 fi
